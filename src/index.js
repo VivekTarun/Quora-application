3c20ba62194e4e8341500596226ec6a8aa2ab84b
vivektarun <vivektarun1234@gmail.com>
2024-04-17 18:41:01 +0530

    feat: error handler file add and called in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/server.config');
 const connectToDB = require('./config/db.config');
+const errorHandler = require('./utils/errorHandler');
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,6 +12,8 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.get('/ping', (req, res) => {
     return res.json({messge : 'Quora application is alive'});
 })
+
+app.use(errorHandler);
 
 app.listen(PORT, async () => {
     console.log(`server started at port ${PORT}`);
